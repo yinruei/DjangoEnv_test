19c32eda094856394438bb553e4da1535c84d3b7
蘇胤瑞 <yinruei@g.ncu.edu.tw>
2018-10-14 02:18:20 +0800

    update ch05

--- ch05/students/studentsapp/views.py
+++ ch05/students/studentsapp/views.py
@@ -2,17 +2,17 @@ from django.shortcuts import render,redirect
 from studentsapp.models import student
 
 def listone(request): 
-	try: 
-		unit = student.objects.get(cName="李采茜") #讀取一筆資料
+    try: 
+        unit = student.objects.get(cName="李采茜") #讀取一筆資料
         # unit = student.objects.get(cPhone= "0914530768")
-	except:
-         errormessage = " (讀取錯誤!)"
-	return render(request, "listone.html", locals())
+    except:
+        errormessage = "(讀取錯誤!)"
+    return render(request, "listone.html", locals())
 
 def listall(request):  
     students = student.objects.all().order_by('id')  #讀取資料表, 依 id 遞增排序
     return render(request, "listall.html", locals())
-	
+
 def index(request):  
     students = student.objects.all().order_by('id')  #讀取資料表, 依 id 遞增排序
     return render(request, "index.html", locals())	
