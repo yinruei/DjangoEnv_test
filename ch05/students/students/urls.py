fa7d8a20eac9cc5537da6da3b93e4adaf8aa70df
yinruei <yinruei@cloudybay.com.tw>
2018-10-15 17:14:18 +0800

    finish ch05

--- ch05/students/students/urls.py
+++ ch05/students/students/urls.py
@@ -12,11 +12,11 @@ urlpatterns = [
     url(r'^post/$', views.post),#POST傳送表單
     url(r'^post1/$', views.post1),#資料新增，資料不驗證
     url(r'^post2/$', views.post2),#資料新增，資料不驗證
-    # url(r'^delete/(\d+)/$', views.delete),
+    url(r'^delete/(\d+)/$', views.delete),
 
-    # url(r'^edit/(\d+)/$', views.edit),#由瀏覽器瀏覽
-    # url(r'^edit/(\d+)/(\w+)$', views.edit),#由edit.html按送出鈕
+    url(r'^edit/(\d+)/$', views.edit),#由瀏覽器瀏覽
+    url(r'^edit/(\d+)/(\w+)$', views.edit),#由edit.html按送出鈕
 
-    # url(r'^edit2/(\d+)/(\w+)$', views.edit2),
+    url(r'^edit2/(\d+)/(\w+)$', views.edit2),
     url(r'^postform/$', views.postform),#表單驗證 
 ]
--- ch05/students/studentsapp/views.py
+++ ch05/students/studentsapp/views.py
@@ -47,23 +47,80 @@ def postform(request):  #新增資料，資料必須驗證
     return render(request, "postform.html", locals())
 
 def post2(request):  #新增資料，資料必須驗證
-	if request.method == "POST":  #如果是以POST方式才處理
-		postform = PostForm(request.POST)  #建立forms物件
-		if postform.is_valid():			#通過forms驗證
-			cName = postform.cleaned_data['cName'] #取得表單輸入資料
-			cSex =  postform.cleaned_data['cSex']
-			cBirthday =  postform.cleaned_data['cBirthday']
-			cEmail = postform.cleaned_data['cEmail']
-			cPhone =  postform.cleaned_data['cPhone']
-			cAddr =  postform.cleaned_data['cAddr']
-			#新增一筆記錄
-			unit = student.objects.create(cName=cName, cSex=cSex, cBirthday=cBirthday, cEmail=cEmail,cPhone=cPhone, cAddr=cAddr) 
-			unit.save()  #寫入資料庫
-			message = '已儲存...'
-			return redirect('/index/')	
-		else:
-			message = '驗證碼錯誤！'	
-	else:
-		message = '姓名、性別、生日必須輸入！'
-		postform = PostForm()
-	return render(request, "post2.html", locals())
+    if request.method == "POST":  #如果是以POST方式才處理
+        postform = PostForm(request.POST)  #建立forms物件
+        if postform.is_valid():			#通過forms驗證
+            cName = postform.cleaned_data['cName'] #取得表單輸入資料
+            cSex =  postform.cleaned_data['cSex']
+            cBirthday =  postform.cleaned_data['cBirthday']
+            cEmail = postform.cleaned_data['cEmail']
+            cPhone =  postform.cleaned_data['cPhone']
+            cAddr =  postform.cleaned_data['cAddr']
+            #新增一筆記錄
+            unit = student.objects.create(cName=cName, cSex=cSex, cBirthday=cBirthday, cEmail=cEmail,cPhone=cPhone, cAddr=cAddr) 
+            unit.save()  #寫入資料庫
+            message = '已儲存...'
+            return redirect('/index/')	
+        else:
+            message = '驗證碼錯誤！'	
+    else:
+        message = '姓名、性別、生日必須輸入！'
+        postform = PostForm()
+        return render(request, "post2.html", locals())
+
+def delete(request,id=None):#刪除資料
+    if id!=None:
+        if request.method=="POST":#如果是以POST方式處理
+            id=request.POST['cId']#取得表單輸入的單號
+        try:
+            unit = student.objects.get(id=id)
+            unit.delete()
+            return redirect('/index/')
+        except:
+            message = "讀取錯誤!"
+    return render(request, "delete.html", locals())
+
+def edit(request,id=None,mode=None):
+    if mode == "edit":#由edit.html按submit
+        unit = student.objects.get(id=id)#取得要修改的資料紀錄
+        unit.cName = request.GET['cName']
+        unit.cSex  = request.GET['cSex']
+        unit.cBirthday = request.GET['cBirthday']
+        unit.cEmail = request.GET['cEmail']
+        unit.cPhone = request.GET['cPhone']
+        unit.cAddr  = request.GET['cAddr']
+        unit.save()#寫入資料庫
+        message = '已修改....'
+        return redirect('/index/')
+    else:#由網址列
+        try:
+            unit = student.objects.get(id=id)#取得要修改的資料紀錄
+            strdate  = str(unit.cBirthday)
+            strdate2 = strdate.replace("年","-")
+            strdate2 = strdate.replace("月","-")
+            strdate2 = strdate.replace("日","-")
+            unit.cBirthday = strdate2
+        except:
+            message = "此id不存在"
+    return render(request, "edit.html", locals())
+
+def edit2(request,id=None,mode=None):
+    if mode == "load":  # 由 index.html 按 編輯二 鈕
+        unit = student.objects.get(id=id)  #取得要修改的資料記
+        strdate=str(unit.cBirthday)
+        strdate2=strdate.replace("年","-")
+        strdate2=strdate.replace("月","-")
+        strdate2=strdate.replace("日","-")
+        unit.cBirthday = strdate2		
+        return render(request, "edit2.html", locals())
+    elif mode == "save": # 由 edit2.html 按 submit		
+        unit = student.objects.get(id=id)  #取得要修改的資料記錄	
+        unit.cName=request.POST['cName']
+        unit.cSex=request.POST['cSex']
+        unit.cBirthday=request.POST['cBirthday']
+        unit.cEmail=request.POST['cEmail']
+        unit.cPhone=request.POST['cPhone']
+        unit.cAddr=request.POST['cAddr']		
+        unit.save()  #寫入資料庫
+        message = '已修改...'
+        return redirect('/index/')
