5f9d5e346e5c28c86d3d33a58797f293c45fbe74
yinruei <yinruei@cloudybay.com.tw>
2019-01-14 18:21:08 +0800

    update

--- ch06/CookieSession/CookieSessionApp/views.py
+++ ch06/CookieSession/CookieSessionApp/views.py
@@ -5,7 +5,7 @@ import datetime
 
 def set_cookie(request,key=None,value=None):
     response = HttpResponse('Cookie 儲存完畢!')
-    response.set_cookie(key,value)
+    response.set_cookie(key,value,max_age=1000)
     return response
 
 def get_cookie(request,key=None,value=None):
@@ -14,18 +14,19 @@ def get_cookie(request,key=None,value=None):
     else:
         return HttpResponse('Cookie 不存在!')
 
+
 def get_allcookies(request):
-    if request.COOKIES !=None:
-        strcookies=""
-        for key1,value1 in request.COOKIES.items():
-            strcookies=strcookies + key1 + ":" + value1 +"<br>"
-        return HttpResponse('%s' %(strcookies))
+    if request.COOKIES != None:
+        strcookies = ""
+        for key1, value1 in request.COOKIES.items():
+            strcookies = strcookies + key1 + ':' + value1 +'<br>'
+        return HttpResponse(' %s ' %(strcookies))
     else:
         return HttpResponse('Cookie 不存在!')
 
 def set_cookie2(request,key=None,value=None):
     response = HttpResponse('Cookie 有效時間')
-    response.set_cookie(key,value,max_age=3600)
+    response.set_cookie(key, value, max_age=3600)
     return response
 
 def delete_cookie(request,key=None,value=None):
@@ -43,9 +44,23 @@ def index(request):
     else:
         counter=1
     response =HttpResponse('今日瀏覽次數: '+ str(counter))
+    print(datetime.datetime.now())
     tomorrow = datetime.datetime.now()+ datetime.timedelta(days=1)
+    print(tomorrow)
     tomorrow = datetime.datetime.replace(tomorrow,hour=0,minute=0,second=0)
+    print(tomorrow)
     expires = datetime.datetime.strftime(tomorrow, "%a, %d-%b-%y %H:%M:%S GMT")
     print(expires)
     response.set_cookie("counter",counter,expires=expires)
     return response
+
+def set_session(request, key=None, value= None):
+    response = HttpResponse('Session儲存完畢!')
+    request.session[key] = value
+    return response
+
+def get_session(request, key=None):
+    if key in request.session:
+        return HttpResponse('%s : %s' % (key, request.session[key]))
+    else:
+        return HttpResponse('Session不存在!')
